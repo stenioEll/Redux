059b0f3956f13b0910ed920b8f42d7c7a0479205
Stênio Ellison <39472541+stenioEll@users.noreply.github.com>
2023-06-21 17:42:03 -0300

    Studies of redux

---
+++ index.js
@@ -0,0 +1,15 @@
+const  CAKE_ORDERED = 'CAKE_ORDERED'
+
+function orderCake() {
+    return {
+        type: CAKE_ORDERED,
+        quantity:1
+    } 
+
+}
+
+numOfCakes = 10
+ 
+
+//(previousState, action) => new State
+
