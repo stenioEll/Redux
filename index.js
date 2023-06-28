e9f9737bbfcb5f9bc22eae49d559db56f8b553bb
Stênio Ellison <39472541+stenioEll@users.noreply.github.com>
2023-06-28 17:22:49 -0300

    commit

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
+const redux = require('redux')
+const createStore = redux.createStore
+
 const  CAKE_ORDERED = 'CAKE_ORDERED'
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
+const bindActionCreators = redux.bindActionCreators
 
 function orderCake() {
     return {
         type: CAKE_ORDERED,
-        quantity:1
+        payload:1
+    } 
+
+}
+
+function restockCake( qty = 1 ) {
+    return {
+        type: CAKE_RESTOCKED,
+        payload: qty
     } 
 
 }
 
 const initialState = {
     numOfCakes: 10, 
-    anotherProperty: 0, 
-
 }
  
 
@@ -23,8 +34,28 @@ const reducer = (state = initialState, action) => {
                 ...state, //copy of state property and chage only the property i wanna
                 numOfCakes: state.numOfCakes - 1
             }
-            default:
-                return state
+        case CAKE_RESTOCKED:
+            return {
+                ...state, 
+                numOfCakes: state.numOfCakes + action.payload
+            }
+        default:
+            return state
     }
 }
+
+const store = createStore(reducer) //store hold the state aplication
+
+console.log('Initial state', store.getState()) //Allow acess of inicial state
+
+const unsubscribe = store.subscribe(() => console.log('update state', store.getState()))
+
+const actions = bindActionCreators({ orderCake, restockCake}, store.dispatch)
+actions.orderCake()
+actions.orderCake()
+actions.orderCake()
+actions.restockCake(3)
+
+unsubscribe()
+
 
